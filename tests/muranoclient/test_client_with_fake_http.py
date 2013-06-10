be1bcaf241825477f27faa9f8ac646b02a451fa5
Serg Melikyan <smelikyan@mirantis.com>
2013-06-10 17:09:49 +0400

    Added tests for new services
    Change-Id: I08afb1b358292782bab069a93de18049ae3470fa

--- tests/muranoclient/test_client_with_fake_http.py
+++ tests/muranoclient/test_client_with_fake_http.py
@@ -204,3 +204,265 @@ class UnitTestsForClassesAndFunctions(unittest.TestCase):
 
         result = test_client.webServers.list('1', 'test')
         assert result == []
+
+    @httprettified
+    def test_client_aspapp_list(self):
+        body = '''
+{
+    "aspNetApps":
+    [
+        {
+            "id": "88f6ed99ff3645bcb84e1e37ab9ece3d",
+            "name": "frontend",
+            "created": "2010-11-30T03:23:42Z",
+            "updated": "2010-11-30T03:23:44Z",
+            "domain": "ACME",
+            "repository": "https://github.com/Mirantis/murano-mvc-demo.git",
+            "uri": "http://10.0.0.2",
+            "units": [{
+                "id": "59255829f0574297acc1cd3a18ff6fd7",
+                "address": "10.0.0.2",
+                "location": "west-dc"
+            }]
+        }, {
+            "id": "aa49dcaff9914b8abb26855f0799b0e0",
+            "name": "backend",
+            "created": "2010-11-30T03:23:42Z",
+            "updated": "2010-11-30T03:23:44Z",
+            "repository": "https://github.com/Mirantis/murano-mvc-demo.git",
+            "uri": "http://10.0.0.3",
+            "domain": "ACME2" ,
+            "units": [{
+                "id": "274b54f6bbe6493690e7107aa947e112",
+                "address": "10.0.0.3",
+                "location": "west-dc"
+            }]
+        }
+    ]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetApps'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetApps.list('1', 'test')
+        assert result[0].name == 'frontend'
+
+    @httprettified
+    def test_client_aspapp_create(self):
+        body = '''
+{
+    "id": "88f6ed99ff3645bcb84e1e37ab9ece3d",
+    "name": "frontend",
+    "created": "2010-11-30T03:23:42Z",
+    "updated": "2010-11-30T03:23:44Z",
+    "domain": "ACME",
+    "repository": "https://github.com/Mirantis/murano-mvc-demo.git",
+    "uri": "http://10.0.0.2",
+    "units": [{
+        "id": "59255829f0574297acc1cd3a18ff6fd7",
+        "address": "10.0.0.2",
+        "location": "west-dc"
+    }]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetApps'
+        HTTPretty.register_uri(HTTPretty.POST, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetApps.create('1', 'test', 'test')
+        assert result.name == 'frontend'
+
+    @httprettified
+    def test_client_aspapp_list_without_elements(self):
+        body = '{"aspNetApps": []}'
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetApps'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetApps.list('1', 'test')
+        assert result == []
+
+    @httprettified
+    def test_client_webfarm_list(self):
+        body = '''
+{
+    "webServerFarms":
+    [
+        {
+            "id": "01fa4412ab4849acb27394aaf307ca88",
+            "name": "frontend",
+            "created": "2010-11-30T03:23:42Z",
+            "updated": "2010-11-30T03:23:44Z",
+            "domain": "ACME",
+            "loadBalancerPort": 80,
+            "uri": "http://192.168.1.1:80",
+            "units":
+            [
+                {
+                    "id": "a34992c8634b482798187d3c0e1c999a",
+                    "address": "10.0.0.2",
+                    "location": "west-dc"
+                },
+                {
+                    "id": "fcd60488bb6f4acf97ccdb8f8fc6bc9a",
+                    "address": "10.0.0.3",
+                    "location": "west-dc"
+                }
+            ]
+        }
+    ]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/webServerFarms'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.webServerFarms.list('1', 'test')
+        assert result[0].name == 'frontend'
+
+    @httprettified
+    def test_client_webfarm_create(self):
+        body = '''
+{
+    "name": "frontend",
+    "domain": "ACME",
+    "loadBalancerPort": 80,
+    "uri": "http://192.168.1.1:80",
+    "units":
+    [
+        {"location": "west-dc"},
+        {"location": "west-dc"}
+    ]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/webServerFarms'
+        HTTPretty.register_uri(HTTPretty.POST, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.webServerFarms.create('1', 'test', 'test')
+        assert result.name == 'frontend'
+
+    @httprettified
+    def test_client_webfarm_list_without_elements(self):
+        body = '{"webServerFarms": []}'
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/webServerFarms'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.webServerFarms.list('1', 'test')
+        assert result == []
+
+    @httprettified
+    def test_client_aspappfarm_list(self):
+        body = '''
+{
+    "aspNetAppFarms":
+    [
+        {
+            "id": "01fa4412ab4849acb27394aaf307ca88",
+            "name": "frontend",
+            "created": "2010-11-30T03:23:42Z",
+            "updated": "2010-11-30T03:23:44Z",
+            "domain": "ACME",
+            "loadBalancerPort": 80,
+            "uri": "http://192.168.1.1:80",
+            "units":
+            [
+                {
+                    "id": "3374f4eb850e4b27bf734649d7004cc0",
+                    "address": "10.0.0.2",
+                    "location": "west-dc"
+                },
+                {
+                    "id": "fcd60488bb6f4acf97ccdb8f8fc6bc9a",
+                    "address": "10.0.0.3",
+                    "location": "west-dc"
+                }
+            ]
+        }
+    ]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetAppFarms'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetAppFarms.list('1', 'test')
+        assert result[0].name == 'frontend'
+
+    @httprettified
+    def test_client_aspappfarm_create(self):
+        body = '''
+{
+    "name": "frontend",
+    "adminPassword": "password",
+    "domain": "acme.dc",
+    "loadBalancerPort": 80,
+    "repository": "https://github.com/Mirantis/murano-mvc-demo.git",
+    "units": [{
+        "location": "west-dc"
+    }]
+}
+'''
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetAppFarms'
+        HTTPretty.register_uri(HTTPretty.POST, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetAppFarms.create('1', 'test', 'test')
+        assert result.name == 'frontend'
+
+    @httprettified
+    def test_client_aspappfarm_list_without_elements(self):
+        body = '{"aspNetAppFarms": []}'
+        url = 'http://no-resolved-host:8001/environments' \
+              '/1/aspNetAppFarms'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=body,
+                               adding_headers={
+                                   'Content-Type': 'application/json', })
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        result = test_client.aspNetAppFarms.list('1', 'test')
+        assert result == []
