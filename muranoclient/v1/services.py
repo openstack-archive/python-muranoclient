58c22cfa721a95512293ecb98b059c25cf6ac109
Serg Melikyan <smelikyan@mirantis.com>
2013-07-15 16:41:50 +0400

    Added support for new API v0.2
    Change-Id: Iee623ff657f7cfab4c0a684aaa04f39de1e31c60

--- muranoclient/v1/services.py
+++ muranoclient/v1/services.py
@@ -11,200 +11,61 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from functools import wraps
 
 from muranoclient.common import base
 
 
-class ActiveDirectory(base.Resource):
+def normalize_path(f):
+    @wraps(f)
+    def f_normalize_path(*args, **kwargs):
+        if kwargs['path'][0] == '/':
+            kwargs['path'] = kwargs['path'][1:]
+        return f(*args, **kwargs)
+
+    return f_normalize_path
+
+
+class Service(base.Resource):
     def __repr__(self):
-        return '<ActiveDirectory %s>' % self._info
+        return '<Service %s>' % self._info
 
     def data(self, **kwargs):
         return self.manager.data(self, **kwargs)
 
 
-class ActiveDirectoryManager(base.Manager):
-    resource_class = ActiveDirectory
+class ServiceManager(base.Manager):
+    resource_class = Service
 
-    def list(self, environment_id, session_id=None):
+    @normalize_path
+    def get(self, environment_id, path, session_id=None):
         if session_id:
             headers = {'X-Configuration-Session': session_id}
         else:
             headers = {}
 
-        return self._list('environments/{id}/activeDirectories'.
-                          format(id=environment_id),
-                          'activeDirectories',
-                          headers=headers)
+        return self._list('environments/{0}/services/{1}'.
+                          format(environment_id, path), headers=headers)
 
-    def create(self, environment_id, session_id, active_directory):
+    @normalize_path
+    def post(self, environment_id, path, data, session_id):
         headers = {'X-Configuration-Session': session_id}
 
-        return self._create('environments/{id}/activeDirectories'.
-                            format(id=environment_id),
-                            active_directory,
+        return self._create('environments/{id}/services/{1}'.
+                            format(environment_id, path), data,
                             headers=headers)
 
-    def delete(self, environment_id, service_id, session_id):
+    @normalize_path
+    def put(self, environment_id, path, data, session_id):
         headers = {'X-Configuration-Session': session_id}
-        path = 'environments/{id}/activeDirectories/{active_directory_id}'
-        path = path.format(id=environment_id, active_directory_id=service_id)
+
+        return self._update('environments/{id}/services/{1}'.
+                            format(environment_id, path), data,
+                            headers=headers)
+
+    @normalize_path
+    def delete(self, environment_id, path, session_id):
+        headers = {'X-Configuration-Session': session_id}
+        path = 'environments/{0}/services/{1}'.format(environment_id, path)
 
         return self._delete(path, headers=headers)
-
-
-class WebServer(base.Resource):
-    def __repr__(self):
-        return '<WebServer %s>' % self._info
-
-    def data(self, **kwargs):
-        return self.manager.data(self, **kwargs)
-
-
-class WebServerManager(base.Manager):
-    resource_class = WebServer
-
-    def list(self, environment_id, session_id=None):
-        if session_id:
-            headers = {'X-Configuration-Session': session_id}
-        else:
-            headers = {}
-
-        return self._list('environments/{id}/webServers'.
-                          format(id=environment_id),
-                          'webServers',
-                          headers=headers)
-
-    def create(self, environment_id, session_id, web_server):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._create('environments/{id}/webServers'.
-                            format(id=environment_id),
-                            web_server,
-                            headers=headers)
-
-    def delete(self, environment_id, service_id, session_id):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._delete('environments/{id}/webServers/{web_server_id}'
-                            .format(id=environment_id,
-                                    web_server_id=service_id),
-                            headers=headers)
-
-
-class AspNetApp(base.Resource):
-    def __repr__(self):
-        return '<AspNetApp %s>' % self._info
-
-    def data(self, **kwargs):
-        return self.manager.data(self, **kwargs)
-
-
-class AspNetAppManager(base.Manager):
-    resource_class = AspNetApp
-
-    def list(self, environment_id, session_id=None):
-        if session_id:
-            headers = {'X-Configuration-Session': session_id}
-        else:
-            headers = {}
-
-        return self._list('environments/{id}/aspNetApps'.
-                          format(id=environment_id),
-                          'aspNetApps',
-                          headers=headers)
-
-    def create(self, environment_id, session_id, app):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._create('environments/{id}/aspNetApps'.
-                            format(id=environment_id),
-                            app,
-                            headers=headers)
-
-    def delete(self, environment_id, service_id, session_id):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._delete('environments/{id}/aspNetApps/{app_id}'
-                            .format(id=environment_id,
-                                    app_id=service_id),
-                            headers=headers)
-
-
-class WebServerFarm(base.Resource):
-    def __repr__(self):
-        return '<WebServerFarm %s>' % self._info
-
-    def data(self, **kwargs):
-        return self.manager.data(self, **kwargs)
-
-
-class WebServerFarmManager(base.Manager):
-    resource_class = WebServerFarm
-
-    def list(self, environment_id, session_id=None):
-        if session_id:
-            headers = {'X-Configuration-Session': session_id}
-        else:
-            headers = {}
-
-        return self._list('environments/{id}/webServerFarms'.
-                          format(id=environment_id),
-                          'webServerFarms',
-                          headers=headers)
-
-    def create(self, environment_id, session_id, web_server_farm):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._create('environments/{id}/webServerFarms'.
-                            format(id=environment_id),
-                            web_server_farm,
-                            headers=headers)
-
-    def delete(self, environment_id, service_id, session_id):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._delete('environments/{id}/webServerFarms'
-                            '/{web_server_farm_id}'
-                            .format(id=environment_id,
-                                    web_server_farm_id=service_id),
-                            headers=headers)
-
-
-class AspNetAppFarm(base.Resource):
-    def __repr__(self):
-        return '<AspNetAppFarm %s>' % self._info
-
-    def data(self, **kwargs):
-        return self.manager.data(self, **kwargs)
-
-
-class AspNetAppFarmManager(base.Manager):
-    resource_class = AspNetAppFarm
-
-    def list(self, environment_id, session_id=None):
-        if session_id:
-            headers = {'X-Configuration-Session': session_id}
-        else:
-            headers = {}
-
-        return self._list('environments/{id}/aspNetAppFarms'.
-                          format(id=environment_id),
-                          'aspNetAppFarms',
-                          headers=headers)
-
-    def create(self, environment_id, session_id, app_farm):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._create('environments/{id}/aspNetAppFarms'.
-                            format(id=environment_id),
-                            app_farm,
-                            headers=headers)
-
-    def delete(self, environment_id, service_id, session_id):
-        headers = {'X-Configuration-Session': session_id}
-
-        return self._delete('environments/{id}/aspNetAppFarms/{app_id}'
-                            .format(id=environment_id,
-                                    app_id=service_id),
-                            headers=headers)
---
+++ tests/test_methods.py
@@ -0,0 +1,235 @@
+#    Copyright (c) 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest2 as unittest
+import logging
+from mock import MagicMock
+
+from muranoclient.client import Client
+import muranoclient.v1.environments as environments
+import muranoclient.v1.services as services
+import muranoclient.v1.sessions as sessions
+
+
+def my_mock(*a, **b):
+    return [a, b]
+
+
+LOG = logging.getLogger('Unit tests')
+api = MagicMock(json_request=my_mock)
+
+
+class UnitTestsForClassesAndFunctions(unittest.TestCase):
+    def test_create_client_instance(self):
+
+        endpoint = 'http://no-resolved-host:8001'
+        test_client = Client('1', endpoint=endpoint, token='1', timeout=10)
+
+        assert test_client.environments is not None
+        assert test_client.sessions is not None
+        assert test_client.services is not None
+
+    def test_env_manager_list(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.list()
+        assert result == []
+
+    def test_env_manager_create(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.create('test')
+        assert result.body == {'name': 'test'}
+
+    def test_env_manager_create_with_named_parameters(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.create(name='test')
+        assert result.body == {'name': 'test'}
+
+    def test_env_manager_create_negative_without_parameters(self):
+        result = 'Exception'
+        manager = environments.EnvironmentManager(api)
+        try:
+            result = manager.create()
+        except TypeError:
+            pass
+        assert result is 'Exception'
+
+    def test_env_manager_delete(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.delete('test')
+        assert result is None
+
+    def test_env_manager_delete_with_named_parameters(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.delete(environment_id='1')
+        assert result is None
+
+    def test_env_manager_delete_negative_without_parameters(self):
+        result = 'Exception'
+        manager = environments.EnvironmentManager(api)
+        try:
+            result = manager.delete()
+        except TypeError:
+            pass
+        assert result is 'Exception'
+
+    def test_env_manager_update(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.update('1', 'test')
+        assert result.body == {'name': 'test'}
+
+    def test_env_manager_update_with_named_parameters(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.update(environment_id='1',
+                                name='test')
+        assert result.body == {'name': 'test'}
+
+    def test_env_manager_update_negative_with_one_parameter(self):
+        result = 'Exception'
+        manager = environments.EnvironmentManager(api)
+        try:
+            result = manager.update('test')
+        except TypeError:
+            pass
+        assert result is 'Exception'
+
+    def test_env_manager_update_negative_without_parameters(self):
+        result = 'Exception'
+        manager = environments.EnvironmentManager(api)
+        try:
+            result = manager.update()
+        except TypeError:
+            pass
+        assert result is 'Exception'
+
+    def test_env_manager_get(self):
+        manager = environments.EnvironmentManager(api)
+        result = manager.get('test')
+        ## WTF?
+        assert result.manager is not None
+
+    def test_env(self):
+        environment = environments.Environment(api, api)
+        assert environment.data() is not None
+
+    def test_session_manager_delete(self):
+        manager = sessions.SessionManager(api)
+        result = manager.delete('datacenter1', 'session1')
+        assert result is None
+
+    def test_session_manager_delete_with_named_parameters(self):
+        manager = sessions.SessionManager(api)
+        result = manager.delete(environment_id='datacenter1',
+                                session_id='session1')
+        assert result is None
+
+    def test_session_manager_delete_negative_with_one_parameter(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.delete('datacenter1')
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_delete_negative_without_parameters(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.delete()
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_get(self):
+        manager = sessions.SessionManager(api)
+        result = manager.get('datacenter1', 'session1')
+        # WTF?
+        assert result.manager is not None
+
+    def test_session_manager_configure(self):
+        manager = sessions.SessionManager(api)
+        result = manager.configure('datacenter1')
+        assert result is not None
+
+    def test_session_manager_configure_with_named_parameter(self):
+        manager = sessions.SessionManager(api)
+        result = manager.configure(environment_id='datacenter1')
+        assert result is not None
+
+    def test_session_manager_configure_negative_without_parameters(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.configure()
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_deploy(self):
+        manager = sessions.SessionManager(api)
+        result = manager.deploy('datacenter1', '1')
+        assert result is None
+
+    def test_session_manager_deploy_with_named_parameters(self):
+        manager = sessions.SessionManager(api)
+        result = manager.deploy(environment_id='datacenter1',
+                                session_id='1')
+        assert result is None
+
+    def test_session_manager_deploy_negative_with_one_parameter(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.deploy('datacenter1')
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_deploy_negative_without_parameters(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.deploy()
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_reports(self):
+        manager = sessions.SessionManager(api)
+        result = manager.reports('datacenter1', '1')
+        assert result == []
+
+    def test_session_manager_reports_with_named_parameters(self):
+        manager = sessions.SessionManager(api)
+        result = manager.reports(environment_id='datacenter1',
+                                 session_id='1')
+        assert result == []
+
+    def test_session_manager_reports_negative_with_one_parameter(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.reports('datacenter1')
+        except TypeError:
+            pass
+        assert result == 'Exception'
+
+    def test_session_manager_reports_negative_without_parameters(self):
+        result = 'Exception'
+        manager = sessions.SessionManager(api)
+        try:
+            result = manager.reports()
+        except TypeError:
+            pass
+        assert result == 'Exception'
