448eb591ed82f766ff32f4cabb00a09e4d29dd7c
Tatyana Kuterina <tkuterina@mirantis.com>
2016-02-25 14:24:16 +0300

    Add test to check error message for bundle-import with non-existed package
    Test case checks that error message after calling bundle-import with
    non-existed packages name inside contains user friendly substring
    
    Targets: blueprint murano-cli-integration-test-coverage
    
    Change-Id: Ied27fcee117750158a98eb4f367865fbf1a13d61

--- muranoclient/tests/functional/cli/test_murano.py
+++ muranoclient/tests/functional/cli/test_murano.py
@@ -678,3 +678,16 @@ class BundleMuranoSanityClientTest(utils.CLIUtilsTestPackagesBase):
         result = self.murano('bundle-import', params='',
                              fail_ok=True)
         self.assertIn("murano bundle-import: error: too few arguments", result)
+
+    def test_bundle_import_with_non_existing_package_name(self):
+        """Test scenario:
+            1) Execute murano bundle-import command with non-existing packages
+            name inside
+            2) check that error message contains user friendly substring
+        """
+        result = self.murano(
+            'bundle-import',
+            params=self.prepare_bundle_with_non_existed_package(),
+            fail_ok=False)
+        self.assertIn("Couldn't find file for package", result)
+        self.assertIn("Error Got non-ok status(404) while connecting", result)
