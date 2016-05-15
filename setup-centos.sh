e39b1893680595d9e3fe436f651d5842aa97e3f3
venkatamahesh <venkatamaheshkotha@gmail.com>
2016-05-15 21:19:38 +0530

    Improve muranoclient install scripts syntax
    There are few trailing spaces and also some tab indents
    are corrected
    
    Change-Id: I66aed3324466226e378f2d7b2501e0510bbad7d3
    Closes-Bug: #1581980
    (cherry picked from commit 3c36f1b70d5dea0a80668dbd861651eadcc970cf)

--- setup-centos.sh
+++ setup-centos.sh
@@ -26,27 +26,27 @@ GIT_CLONE_DIR=`echo $SERVICE_CONTENT_DIRECTORY | sed -e "s/$SERVICE_SRV_NAME//"`
 # Loger function
 log()
 {
-	MSG=$1
-	if [ $LOGLVL -gt 0 ]; then
-		echo "LOG:> $MSG"
-	fi
+        MSG=$1
+        if [ $LOGLVL -gt 0 ]; then
+                echo "LOG:> $MSG"
+        fi
 }
 
 # Check or install package
 in_sys_pkg()
 {
-	PKG=$1
-	rpm -q $PKG > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-	    log "Package \"$PKG\" already installed"
-	else
-		log "Installing \"$PKG\"..."
-		yum install $PKG --assumeyes > /dev/null 2>&1
-		if [ $? -ne 0 ];then
-			log "installation fails, exiting!!!"
-			exit
-		fi
-	fi
+        PKG=$1
+        rpm -q $PKG > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            log "Package \"$PKG\" already installed"
+        else
+                log "Installing \"$PKG\"..."
+                yum install $PKG --assumeyes > /dev/null 2>&1
+                if [ $? -ne 0 ];then
+                        log "installation fails, exiting!!!"
+                        exit
+                fi
+        fi
 }
 
 # find pip
@@ -70,14 +70,14 @@ find_pip()
 # git clone
 gitclone()
 {
-	FROM=$1
-	CLONEROOT=$2
-	log "Cloning from \"$FROM\" repo to \"$CLONEROOT\""
-	cd $CLONEROOT && git clone $FROM > /dev/null 2>&1
-	if [ $? -ne 0 ];then
-	    log "cloning from \"$FROM\" fails, exiting!!!"
-	    exit
-	fi
+        FROM=$1
+        CLONEROOT=$2
+        log "Cloning from \"$FROM\" repo to \"$CLONEROOT\""
+        cd $CLONEROOT && git clone $FROM > /dev/null 2>&1
+        if [ $? -ne 0 ];then
+            log "cloning from \"$FROM\" fails, exiting!!!"
+            exit
+        fi
 }
 
 # install
@@ -85,94 +85,94 @@ inst()
 {
 CLONE_FROM_GIT=$1
 # Checking packages
-	for PKG in $PREREQ_PKGS
-	do
-		in_sys_pkg $PKG
-	done 
+        for PKG in $PREREQ_PKGS
+        do
+                in_sys_pkg $PKG
+        done
 # Find python pip
-	find_pip
+        find_pip
 # If clone from git set
-	if [ ! -z $CLONE_FROM_GIT ]; then
+        if [ ! -z $CLONE_FROM_GIT ]; then
 # Preparing clone root directory
-	if [ ! -d $GIT_CLONE_DIR ];then
-		log "Creating $GIT_CLONE_DIR directory..."
-		mkdir -p $GIT_CLONE_DIR
-		if [ $? -ne 0 ];then
-			log "Can't create $GIT_CLONE_DIR, exiting!!!" 
-			exit
-		fi
-	fi
+        if [ ! -d $GIT_CLONE_DIR ];then
+                log "Creating $GIT_CLONE_DIR directory..."
+                mkdir -p $GIT_CLONE_DIR
+                if [ $? -ne 0 ];then
+                        log "Can't create $GIT_CLONE_DIR, exiting!!!"
+                        exit
+                fi
+        fi
 # Cloning from GIT
-		GIT_WEBPATH_PRFX="https://github.com/stackforge/"
-		gitclone "$GIT_WEBPATH_PRFX$SERVICE_SRV_NAME.git" $GIT_CLONE_DIR
-# End clone from git section 
-	fi
+                GIT_WEBPATH_PRFX="https://git.openstack.org/cgit/openstack/"
+                gitclone "$GIT_WEBPATH_PRFX$SERVICE_SRV_NAME.git" $GIT_CLONE_DIR
+# End clone from git section
+        fi
 # Setupping...
-	log "Running setup.py"
-	MRN_CND_SPY=$SERVICE_CONTENT_DIRECTORY/setup.py
-	if [ -e $MRN_CND_SPY ]; then
-		chmod +x $MRN_CND_SPY
-		log "$MRN_CND_SPY output:_____________________________________________________________"
+        log "Running setup.py"
+        MRN_CND_SPY=$SERVICE_CONTENT_DIRECTORY/setup.py
+        if [ -e $MRN_CND_SPY ]; then
+                chmod +x $MRN_CND_SPY
+                log "$MRN_CND_SPY output:_____________________________________________________________"
 ## Setup through pip
-		# Creating tarball
-		rm -rf $SERVICE_CONTENT_DIRECTORY/*.egg-info
-		cd $SERVICE_CONTENT_DIRECTORY && python $MRN_CND_SPY egg_info
+                # Creating tarball
+                rm -rf $SERVICE_CONTENT_DIRECTORY/*.egg-info
+                cd $SERVICE_CONTENT_DIRECTORY && python $MRN_CND_SPY egg_info
                 if [ $? -ne 0 ];then
                         log "\"$MRN_CND_SPY\" egg info creation FAILS, exiting!!!"
                         exit 1
                 fi
-		rm -rf $SERVICE_CONTENT_DIRECTORY/dist/*
-		cd $SERVICE_CONTENT_DIRECTORY && python $MRN_CND_SPY sdist
-		if [ $? -ne 0 ];then
-			log "\"$MRN_CND_SPY\" tarball creation FAILS, exiting!!!"
-			exit 1
-		fi
-		# Running tarball install
-		TRBL_FILE=$(basename `ls $SERVICE_CONTENT_DIRECTORY/dist/*.tar.gz`)
-		$PIPCMD install $SERVICE_CONTENT_DIRECTORY/dist/$TRBL_FILE
-		if [ $? -ne 0 ];then
-			log "$PIPCMD install \"$TRBL_FILE\" FAILS, exiting!!!"
-			exit 1
-		fi
-	else
-		log "$MRN_CND_SPY not found!"
-	fi
+                rm -rf $SERVICE_CONTENT_DIRECTORY/dist/*
+                cd $SERVICE_CONTENT_DIRECTORY && python $MRN_CND_SPY sdist
+                if [ $? -ne 0 ];then
+                        log "\"$MRN_CND_SPY\" tarball creation FAILS, exiting!!!"
+                        exit 1
+                fi
+                # Running tarball install
+                TRBL_FILE=$(basename `ls $SERVICE_CONTENT_DIRECTORY/dist/*.tar.gz`)
+                $PIPCMD install $SERVICE_CONTENT_DIRECTORY/dist/$TRBL_FILE
+                if [ $? -ne 0 ];then
+                        log "$PIPCMD install \"$TRBL_FILE\" FAILS, exiting!!!"
+                        exit 1
+                fi
+        else
+                log "$MRN_CND_SPY not found!"
+        fi
 }
 
 # uninstall
 uninst()
 {
-	# Uninstall trough  pip
+        # Uninstall trough  pip
         find_pip
-	# looking up for python package installed
-	PYPKG="muranoclient"
-	_pkg=$($PIPCMD freeze | grep $PYPKG)
-	if [ $? -eq 0 ]; then
-		log "Removing package \"$PYPKG\" with pip"
-		$PIPCMD uninstall $_pkg --yes
-	else
-		log "Python package \"$PYPKG\" not found"
-	fi
+        # looking up for python package installed
+        PYPKG="muranoclient"
+        _pkg=$($PIPCMD freeze | grep $PYPKG)
+        if [ $? -eq 0 ]; then
+                log "Removing package \"$PYPKG\" with pip"
+                $PIPCMD uninstall $_pkg --yes
+        else
+                log "Python package \"$PYPKG\" not found"
+        fi
 }
 # Command line args'
 COMMAND="$1"
 case $COMMAND in
-	install )
-		inst
-		;;
+        install )
+                inst
+                ;;
 
-	installfromgit )
-		inst "yes"
-		;;
+        installfromgit )
+                inst "yes"
+                ;;
 
-	uninstall )
-		log "Uninstalling muranoclient \"$SERVICE_SRV_NAME\" from system..."
-		uninst
-		;;
+        uninstall )
+                log "Uninstalling muranoclient \"$SERVICE_SRV_NAME\" from system..."
+                uninst
+                ;;
 
-	* )
-		echo -e "Usage: $(basename "$0") command \nCommands:\n\tinstall - Install $SERVICE_SRV_NAME software\n\tuninstall - Uninstall $SERVICE_SRV_NAME software"
-		exit 1
-		;;
-esac 
+        * )
+                echo -e "Usage: $(basename "$0") command \nCommands:\n\tinstall - Install $SERVICE_SRV_NAME software\n\tuninstall - Uninstall $SERVICE_SRV_NAME software"
+                exit 1
+                ;;
+esac
 
