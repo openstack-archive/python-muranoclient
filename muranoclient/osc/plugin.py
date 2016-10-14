527d068d7f6d09e251b989bd4e90f42475868ae3
KATO Tomoyuki <kato.tomoyuki@jp.fujitsu.com>
2016-10-14 13:43:29 +0900

    Add plug-in summary for osc doc
    Stevedore Sphinx extension handles this comment.
    http://docs.openstack.org/developer/python-openstackclient/plugin-commands.html
    
    Change-Id: I717993b1e34a7a2bc9f05b90c040f37a434190d6

--- muranoclient/osc/plugin.py
+++ muranoclient/osc/plugin.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+"""OpenStackClient plugin for Application Catalog service."""
+
 from osc_lib import utils
 from oslo_log import log as logging
 
